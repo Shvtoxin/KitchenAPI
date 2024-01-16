8d38d0c7686fb3cc2217a147ffe153a26f706019
Shvtoxin <sasha.shvtoxin@mail.ru>
2024-01-16 13:37:38 +0300

    Сделал Common.Entity

---
+++ Kitchen.Common.Entity/InterfaceDB/IDbWriterContext.cs
@@ -0,0 +1,25 @@
+﻿using TicketSelling.Common.Entity.InterfaceDB;
+
+namespace Kitchen.Common.Entity.InterfaceDB
+{
+    /// <summary>
+    /// Контекст для работы с записями в БД
+    /// </summary>
+    public interface IDbWriterContext
+    {
+        /// <inheritdoc cref="IDbWriter"/>
+        IDbWriter Writer { get; }
+
+        /// <inheritdoc cref="IUnitOfWork"/>
+        IUnitOfWork UnitOfWork { get; }
+
+        /// <inheritdoc cref="IDateTimeProvider"/>
+        IDateTimeProvider DateTimeProvider { get; }
+
+        /// <summary>
+        /// Имя пользователя
+        /// </summary>
+        /// <remarks>В реальной системе с авторизацией тут будет IIdentity</remarks>
+        string UserName { get; }
+    }
+}
