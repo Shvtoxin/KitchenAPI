3b149a4e60ec22bf2783c231dd497f2c9f062778
Shvtoxin <sasha.shvtoxin@mail.ru>
2024-01-16 15:03:46 +0300

    Закончил контекст

---
+++ Kitchen.Context/SampleContextFactory.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Design;
+using Microsoft.Extensions.Configuration;
+
+namespace Kitchen.Context
+{
+    public class SampleContextFactory : IDesignTimeDbContextFactory<KitchenContext>
+    {
+        public KitchenContext CreateDbContext(string[] args)
+        {
+            var configuration = new ConfigurationBuilder()
+                .SetBasePath(Directory.GetCurrentDirectory())
+                .AddJsonFile("appsettings.json")
+                .Build();
+            var connectionString = configuration.GetConnectionString("DefaultConnection");
+
+            var options = new DbContextOptionsBuilder<KitchenContext>()
+                .UseSqlServer(connectionString)
+                .Options;
+
+            return new KitchenContext(options);
+        }
+    }
+}
---
+++ Kitchen.Context/ServiceExtensionsContext.cs
@@ -0,0 +1,25 @@
+﻿using Kitchen.Common.Entity.InterfaceDB;
+using Kitchen.Context.Contracts;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.DependencyInjection.Extensions;
+
+namespace Kitchen.Context
+{
+    /// <summary>
+    /// Методы расширения для <see cref="IServiceCollection"/>
+    /// </summary>
+    public static class ServiceExtensionsContext
+    {
+        /// <summary>
+        /// Регистрирует все что связано с контекстом
+        /// </summary>
+        /// <param name="service"></param>
+        public static void RegistrationContext(this IServiceCollection service)
+        {
+            service.TryAddScoped<IKitchenContext>(provider => provider.GetRequiredService<KitchenContext>());
+            service.TryAddScoped<IDbRead>(provider => provider.GetRequiredService<KitchenContext>());
+            service.TryAddScoped<IDbWriter>(provider => provider.GetRequiredService<KitchenContext>());
+            service.TryAddScoped<IUnitOfWork>(provider => provider.GetRequiredService<KitchenContext>());
+        }
+    }
+}
